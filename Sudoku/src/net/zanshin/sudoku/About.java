ff0d13131961082542480503a9b0bc62a33d2094
Mark Nichols <mark@zanshin.net>
2011-04-24 14:27:00 -0500

    Added basic "About" window to application

---
+++ Sudoku/src/net/zanshin/sudoku/About.java
@@ -0,0 +1,19 @@
+package net.zanshin.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mark
+ * Date: 4/24/11
+ * Time: 2:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
--- Sudoku/src/net/zanshin/sudoku/Sudoku.java
+++ Sudoku/src/net/zanshin/sudoku/Sudoku.java
@@ -2,14 +2,42 @@ package net.zanshin.sudoku;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Sudoku extends Activity
-{
+/**
+ * Created by IntelliJ IDEA.
+ * User: mark
+ * Date: 4/23/11
+ * Time: 10:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sudoku extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // set up click listeners for all the buttons
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.about_button:
+                Intent i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+            // more buttons here later...
+        }
     }
 }
