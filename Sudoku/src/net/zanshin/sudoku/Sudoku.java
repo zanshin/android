7bf3a7284d627a031bfffed1afaad9f7def77d7e
Mark Nichols <mhn@ksu.edu>
2011-04-23 23:09:04 -0500

    Added Sudoku module to project

---
+++ Sudoku/src/net/zanshin/sudoku/Sudoku.java
@@ -0,0 +1,15 @@
+package net.zanshin.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sudoku extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
