6b621240a51f47fc208bc0b74a33d40e0dbd21fb
Mark Nichols <mark@zanshin.net>
2011-04-29 14:34:08 -0500

    created BrowserView module

---
+++ BrowserView/src/net/zanshin/browserview/BrowserView.java
@@ -0,0 +1,15 @@
+package net.zanshin.browserview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BrowserView extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
